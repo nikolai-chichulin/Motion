5bfb4e698d8c09d634a9ffc97b7f00481d66863b
Николай <nikolai.chichulin@gmail.com>
2023-10-20 12:07:03 +0300

    Updated the tests.

--- BullsAndCows/src/bcmain/Tester.java
+++ BullsAndCows/src/bcmain/Tester.java
@@ -5,9 +5,27 @@ import java.util.Locale;
 public class Tester {
 
 	public static void main(String[] args) {
+		// tests();
+		test("7280");
+	}
+
+	public static void tests() {
+		int resmax = 0;
+		int smax = 0;
+		for (int s = 1000; s < 10000; s++) {
+			int res = test(Integer.toString(s));
+			if (res > resmax) {
+				resmax = res;
+				smax = s;
+			}
+		}
+		System.out.println(
+				String.format(Locale.US, "The hardest secret is %d that was solved in %d turns.", smax, resmax));
+	}
+
+	public static int test(String secret) {
 		boolean output = true;
 		Solver solver = new Solver();
-		String secret = "5021";
 		int res = solver.test(secret, output);
 		if (res > 0) {
 			System.out.println(String.format(Locale.US, "%s: wins in %d turns.", secret, res));
@@ -16,6 +34,7 @@ public class Tester {
 		} else {
 			System.out.println(String.format(Locale.US, "Unknown alarm at %s.", secret));
 		}
+		return res;
 	}
 
 }
